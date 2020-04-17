f7cdebc82f083a8c362990755fb9484bf1d37c31
Neil Wu <neilwu0626@gmail.com>
2020-04-17 17:40:24 -0400

    updated docs for rtd

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
@@ -101,7 +101,7 @@ pygments_style = 'sphinx'
 #html_theme = 'default'
  
 html_theme_path = ['themes']
-html_theme = 'mdolab_theme'
+# html_theme = 'mdolab_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
